5d6d768ce86cb2d0137573d4d7d6f5a721e1948e
Dornor <k.suwatchai@hotmail.com>
2024-02-05 15:06:48 +0700

    Start Sprint 2

--- src/app/course/CourseList.jsx
+++ src/app/course/CourseList.jsx
@@ -1,4 +1,4 @@
-import { Link } from "next/link";
+import Link from "next/link";
 
 export default function CourseList({ course }) {
   return (
@@ -12,43 +12,42 @@ export default function CourseList({ course }) {
 
 function Course({ course }) {
   return (
-    <div className="course-container max-w-xs mt-16 mx-3 relative ">
-      <div className="course shadow-[0px_5px_5px_0px_rgba(100,109,137,1)] rounded-md w-[320px] h-[538px]">
-        <div className="course-img ">
-          <img
-            src={course.img_url}
-            alt={course.course_id}
-            className="course-poster-img mb-6 w-[357px] h-[240px] block rounded-t-md"
-          />
-          <span className=" text-[#F47E20] ml-3">Course</span>
-        </div>
-        <div className="course-description-container m-3">
-          <a
-            href={`/
-            course/${course.course_id}`}
-            className="course-name hover:text-[#F47E20] leading-[30px] text-[24px] w-[325px] h-[30px]"
-          >
-            {course.name}
-          </a>
-          <p className="course-description text-[16px] leading-[24px] pb-3 text-[#646D89]">
-            Description {course.description.slice(0, 120)}
-          </p>
-        </div>
-        <div className="course-detail-container  p-[16px] border-t-[1px]  absolute bottom-0 w-[320px]">
-          <span className="mr-3 text-[#646D89]">
-            <i>
-              <img src="icons/book.png" className="inline-block mr-3" />
-            </i>
-            {course.lessons_test.length} Lesson(s)
-          </span>
-          <span className="text-[#646D89]">
-            <i>
-              <img src="icons/clock.png" className="inline-block mr-3 " />
-            </i>
-            {course.length} Hr.
-          </span>
+    <Link href={`/course/${course.course_id}`}>
+      <div className="course-container max-w-xs mt-16 mx-3 relative ">
+        <div className="course shadow-[0px_5px_5px_0px_rgba(100,109,137,1)] rounded-md w-[320px] h-[538px]">
+          <div className="course-img ">
+            <img
+              src={course.img_url}
+              alt={course.course_id}
+              className="course-poster-img mb-6 w-[357px] h-[240px] block rounded-t-md"
+            />
+            <span className=" text-[#F47E20] ml-3">Course</span>
+          </div>
+          <div className="course-description-container m-3 ">
+            <span className="course-name hover:text-[#F47E20] leading-[30px] text-[24px] w-[325px] h-[30px]">
+              {course.name}
+            </span>
+
+            <p className="course-description text-[16px] leading-[24px] pb-3 text-[#646D89]">
+              Description {course.description.slice(0, 120)}
+            </p>
+          </div>
+          <div className="course-detail-container  p-[16px] border-t-[1px]  absolute bottom-0 w-[320px]">
+            <span className="mr-3 text-[#646D89]">
+              <i>
+                <img src="icons/book.png" className="inline-block mr-3" />
+              </i>
+              {course.lessons_test.length} Lesson(s)
+            </span>
+            <span className="text-[#646D89]">
+              <i>
+                <img src="icons/clock.png" className="inline-block mr-3 " />
+              </i>
+              {course.length} Hr.
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }

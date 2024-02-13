969c6bcc49d01bcb1e2b663b272d05917f9d5836
Nok <siriwan.skk@gmail.com>
2024-02-13 17:30:47 +0700

    feat: get api

--- src/app/user/edit_profile/page.jsx
+++ src/app/user/edit_profile/page.jsx
@@ -1,43 +1,54 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import NavBar from "@/components/NavBarTemp";
 import Footer from "@/components/Footer";
 import Button from "@/components/Button";
 import Image from "next/image";
+import { useSession } from "next-auth/react";
+import axios from "axios";
 
 export default function EditProfileForm() {
   const [values, setValues] = useState({
-    fullname: "",
-    password: "",
+    firstname: "",
+    lastname: "",
     dateofBirth: "",
     EducationalBackground: "",
     email: "",
   });
+
+  const [userId, setUserId] = useState([]);
+
+  const [image, setImage] = useState("");
 
   const getValue = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
   };
 
   const [errors, setErrors] = useState({
-    fullname: false,
+    firstname: false,
+    lastname: false,
     dateofBirthempty: false,
     dateofBirth: false,
     email: false,
   });
 
-  const checkName = (value) => {
-    let fullname = value;
-    fullname = fullname
-      .split(" ")
-      .filter((word) => word !== "")
-      .join("");
-
-    console.log(values);
+  const checkFirstName = (value) => {
+    let firstname = value;
+    //console.log(values);
     const englishRegex = /^[A-Za-z]+$/;
 
-    const isValid = englishRegex.test(fullname);
+    const isValid = englishRegex.test(firstname);
+    return isValid;
+  };
+
+  const checkLastName = (value) => {
+    let lastname = value;
+    //console.log(values);
+    const englishRegex = /^[A-Za-z]+$/;
+
+    const isValid = englishRegex.test(lastname);
     return isValid;
   };
 
@@ -63,69 +74,164 @@ export default function EditProfileForm() {
     event.preventDefault();
 
     setErrors({
-      fullname: !checkName(values.fullname),
+      firstname: !checkFirstName(values.firstname),
+      lastname: !checkLastName(values.lastname),
       dateofBirthempty: !values.dateofBirth.trim(),
       dateofBirth: validateDateofBirth(values.dateofBirth),
       EducationalBackground: !values.eb.trim(),
       email: !(values.email.trim() && values.email.includes("@")),
+      image: !values.image.trim(),
     });
   };
+
+  const { data: session, status } = useSession();
+  const [userData, setUserData] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [userProfile, setUserProfile] = useState({});
+  // const []
+
+  const getUser = async () => {
+    console.log(session);
+    //console.log(userData);
+
+    if (status === "authenticated") {
+      const id = session?.user.userId;
+      const res = await axios.get(`/api/user/${id}`);
+      setUserData({
+        ...session.user,
+      });
+      setUserProfile(res.data.data);
+      console.log(res.data.data);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, [status]);
+
+  // async function deleteImage(userId) {
+  //   const id = userId;
+  //   try {
+  //     await axios.delete({ img_url });
+  //     setUserProfile();
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+  // console.log(userProfile);
+  // useEffect(() => {
+  //   getUser();
+  // });
+
+  const deleteImage = (event) => {
+    event.preventDefault();
+    delete userProfile.img_url;
+    setImage({ ...image });
+  };
+
+  // const handleRemoveImage = (event, avatarKey) => {
+  //   event.preventDefault();
+  //   delete avatars[avatarKey];
+  //   setAvatars({ ...avatars });
+  // };
+
+  // async function fetchUserId() {
+  //   if (session?.user.userId != undefined) {
+  //     const id = session?.user.userId;
+  //     const res = await axios.get(`/api/user/${id}`);
+  //     setUserId(res.data.data);
+  //   }
+  // }
+
+  // useEffect(() => fetchUserId(), [session?.user?.userId]);
+  // console.log(session?.user.userId);
+  //console.log(userId);
 
   return (
     <>
       <NavBar />
-      <section className="relative flex gap-0.5 justify-between items-start px-5 w-full h-[955px] max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
+      <section className="relative flex gap-0.5 justify-between items-start px-5 w-full h-[955px] max-md:flex-wrap max-md:mt-10 max-md:max-w-full mx-[auto]">
+        <Image
+          className="absolute pt-[100px] pl-[43px]"
+          src="/images/editProfile.png"
+          alt="editProfile"
+          width={1397}
+          height={190}
+          priority={true}
+        />
         <section className="flex flex-col flex-1 items-center max-md:max-w-full">
           <div className="text-4xl mt-[100px] font-medium tracking-tighter text-black max-md:max-w-full">
             Profile
           </div>
           <div className="flex flex-row w-[930px] h-[531px] mt-[72px]">
-            <div className="bg-blue-100 w-[358px] h-[358px] rounded-[16px]"></div>
-            <form className="flex flex-col pl-[119px]">
+            <div className="bg-blue-100 w-[358px] h-[358px] rounded-[16px] flex justify-center items-center">
+              <img
+                className="absolute rounded-[16px]"
+                src={userProfile?.img_url}
+                alt="image_profile"
+                width={358}
+                height={358}
+              />
+              <button
+                className="absolute left-[590px] top-[223px] w-[32px] h-[32px] rounded-3xl bg-[#9B2FAC] text-white"
+                onClick={() => {
+                  deleteImage(userId);
+                }}>
+                x
+              </button>
+            </div>
+            <form className="absolute flex flex-col pl-[477px]">
               <div className="flex flex-row">
                 <div className="flex flex-col pr-[13px]">
                   <label
-                    htmlFor=" text-black text-[16px] max-md:max-w-full"
-                    className="pb-[4px]">
+                    htmlFor="userId"
+                    className="pb-[4px] text-black text-[16px] max-md:max-w-full">
                     Firstname
                   </label>
                   <input
-                    name="fullname"
+                    id="userId"
+                    name="firstname"
                     onChange={getValue}
-                    value={values.fullname}
-                    // placeholder="Enter Name and Lastname"
+                    value={values.firstname}
+                    placeholder={userProfile?.first_name || "Enter firstName"}
                     className="mb-[40px] w-[220px] h-12 pl-3 pr-4 py-3 bg-white rounded-lg border border-gray-300 justify-start items-start gap-2 inline-flex"></input>
-                  {errors.fullname && (
+                  {errors.firstname && (
                     <span className="text-red-600">กรุณากรอกชื่อ</span>
                   )}
                 </div>
                 <div className="flex flex-col">
                   <label
-                    htmlFor=" text-black text-[16px] max-md:max-w-full"
-                    className="pb-[4px]">
+                    htmlFor="userId"
+                    className="pb-[4px] text-black text-[16px] max-md:max-w-full">
                     Lastname
                   </label>
                   <input
-                    name="fullname"
+                    name="Lastname"
                     onChange={getValue}
-                    value={values.fullname}
-                    // placeholder="Enter Name and Lastname"
+                    value={values.Lastname}
+                    // placeholder={session?.user?.lastName || "Enter lastname"}
+                    placeholder={userProfile?.last_name || "Enter lastName"}
                     className="mb-[40px] w-[220px] h-12 pl-3 pr-4 py-3 bg-white rounded-lg border border-gray-300 justify-start items-start gap-2 inline-flex"></input>
-                  {errors.fullname && (
-                    <span className="text-red-600">กรุณากรอกชื่อ</span>
+                  {errors.Lastname && (
+                    <span className="text-red-600">กรุณากรอกนามสกุล</span>
                   )}
                 </div>
               </div>
-
-              <label htmlFor="text-[16px] mb-[4px]" className="pb-[4px]">
+              <label
+                htmlFor="userId"
+                className="pb-[4px] text-black text-[16px] max-md:max-w-full">
                 Date of Birth
               </label>
+              console.log({userProfile?.date_of_birth}); console.log(
+              {values.dateofBirth});
               <input
                 name="dateofBirth"
                 value={values.dateofBirth}
                 onChange={getValue}
                 type="date"
-                // placeholder="DD/MM/YY"
+                // placeholder={session?.user?.dateofBirth || "Enter DD/MM/YY"}
+                placeholder={userProfile?.date_of_birth || "Enter YYYY-MM-DD"}
                 className="mb-[40px] w-[453px] h-12 pl-3 pr-4 py-3 bg-white rounded-lg border border-gray-300 justify-start items-start gap-2 inline-flex"></input>
               {errors.dateofBirthempty && (
                 <div className="text-red-600">
@@ -137,7 +243,9 @@ export default function EditProfileForm() {
                   You must be at least 6 years old
                 </div>
               )}
-              <label htmlFor="text-[16px] mb-[4px]" className="pb-[4px]">
+              <label
+                htmlFor="userId"
+                className="pb-[4px] text-black text-[16px] max-md:max-w-full">
                 Educational Background
               </label>
               <input
@@ -146,11 +254,17 @@ export default function EditProfileForm() {
                 value={values.EducationalBackground}
                 type="text"
                 // placeholder="Enter Educational Background"
+                placeholder={
+                  userProfile?.educational_background ||
+                  "Enter educational background"
+                }
                 className="mb-[40px] w-[453px] h-12 pl-3 pr-4 py-3 bg-white rounded-lg border border-gray-300 justify-start items-start gap-2 inline-flex"></input>
-              {errors.fullname && (
+              {errors.firstname && (
                 <p className=" mt-[50px]  text-red-600">กรุณากรอกข้อมูล</p>
               )}
-              <label htmlFor="text-[16px] mb-[4px]" className="pb-[4px]">
+              <label
+                htmlFor="userId"
+                className="pb-[4px] text-black text-[16px] max-md:max-w-full">
                 Email
               </label>
               <input
@@ -158,11 +272,11 @@ export default function EditProfileForm() {
                 value={values.email}
                 onChange={getValue}
                 // placeholder="Enter Email"
+                placeholder={session?.user?.email || "Enter email"}
                 className="mb-[40px] w-[453px] h-12 pl-3 pr-4 py-3 bg-white rounded-lg border border-gray-300 justify-start items-start gap-2 inline-flex"></input>
               {errors.password && (
                 <p className=" text-red-600">กรุณากรอก email</p>
               )}
-
               <Button
                 onClick={handleSubmit}
                 className="mb-[40px] w-[453px] h-[60px] px-8 py-[18px] bg-[#2F5FAC] rounded-xl shadow justify-center items-center gap-2.5 inline-flex text-white">
@@ -171,15 +285,8 @@ export default function EditProfileForm() {
             </form>
           </div>
         </section>
-        <Image
-          className="absolute pt-[100px] pl-[43px]"
-          src="/images/editProfile.png"
-          alt="editProfile"
-          width={1397}
-          height={190}
-        />
       </section>
-      <section className="absolute"></section>
+
       <Footer />
     </>
   );

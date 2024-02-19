71dc06c10a8829a960286b29d21c2131622eca01
tanasri <tanawan95@gmail.com>
2024-02-19 14:05:55 +0700

    feat:next,previous btn

--- src/app/user/[courseid]/learning/page.jsx
+++ src/app/user/[courseid]/learning/page.jsx
@@ -14,7 +14,6 @@ import NavBar from "@/components/NavBar";
 import { useEffect, useState } from "react";
 import Footer from "@/components/Footer";
 import { useSession } from "next-auth/react";
-import Button from "@/components/Button.jsx";
 
 export default function Learning({ params }) {
   const { data: session, status } = useSession();
@@ -23,7 +22,7 @@ export default function Learning({ params }) {
   const [isLoading, setIsloading] = useState(false);
   const [subLessonProgress, setSubLessonProgress] = useState([]);
   const [progress, setProgress] = useState(0);
-
+  const [allSubLesson, setAllSubLesson] = useState({});
   useEffect(() => {
     function sumProgress() {
       const progressPercentage = subLessonProgress.map((sub) => sub.status);
@@ -31,7 +30,7 @@ export default function Learning({ params }) {
         progressPercentage.reduce(function (acc, cur) {
           return acc + cur;
         }, 0) / progressPercentage.length;
-      console.log(sumProgress);
+
       setProgress(Math.round(sumProgress));
     }
     sumProgress();
@@ -52,10 +51,13 @@ export default function Learning({ params }) {
         );
 
         setCourseById(res.data.data);
-        console.log(res);
+
         setSubLessonProgress(res?.data?.data[0]?.users_sub_lessons);
         setcurrentSubLesson(res.data.data[0].courses.lessons[0].sub_lessons[0]);
         setIsloading(true);
+        setAllSubLesson(
+          res.data.data[0].courses.lessons.map((lesson) => lesson.sub_lessons)
+        );
       }
     }
     fetchCourseById();
@@ -110,7 +112,26 @@ export default function Learning({ params }) {
     }
   }, [progress]);
 
-  console.log(currentSubLesson);
+  function handleButton(current) {
+    if (allSubLesson) {
+      const allSub = [].concat.apply([], allSubLesson);
+
+      let index;
+      allSub.forEach((id, i) => {
+        if (id.sub_lesson_id == currentSubLesson.sub_lesson_id) {
+          index = i;
+          console.log(index);
+        }
+      });
+      const result = index + current;
+
+      result >= 0 && result < allSub.length
+        ? setcurrentSubLesson(allSub[result])
+        : null;
+    }
+  }
+
+  console.log(courseById[0]);
 
   return (
     <>
@@ -134,12 +155,18 @@ export default function Learning({ params }) {
         )}
       </div>
       <div className="btn-container flex flex-row justify-between w-[1440px] h-[100px] mx-[150px] ">
-        <Button className="text-[#2F5FAC] font-bold w-[162px] h-[60px]">
+        <button
+          className="text-[#2F5FAC] font-bold w-[162px] h-[60px]"
+          onClick={() => handleButton(-1)}
+        >
           Previous Lesson
-        </Button>
-        <Button className="bg-[#2F5FAC] text-white font-bold w-[162px] h-[60px] rounded-lg">
+        </button>
+        <button
+          className="bg-[#2F5FAC] text-white font-bold w-[162px] h-[60px] rounded-lg"
+          onClick={() => handleButton(1)}
+        >
           Next Lesson
-        </Button>
+        </button>
       </div>
       <Footer />
     </>

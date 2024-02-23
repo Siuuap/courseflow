868da0f03fe66613974ea1fab833a2b8af84e300
Dornor <k.suwatchai@hotmail.com>
2024-02-23 16:55:21 +0700

    suspense searchparams

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -5,8 +5,8 @@ import { useRouter, useSearchParams } from "next/navigation";
 import NavBar from "@/components/NavBar";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
-
-export default function Home() {
+import { Suspense } from "react";
+export default function UserLoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
@@ -40,64 +40,68 @@ export default function Home() {
 
   return (
     <>
-      <NavBar />
-      <Image
-        className="absolute pt-[20px] -z-50 "
-        src="/images/assets.png"
-        alt="blackground"
-        width={1459.64}
-        height={839}
-        priority={true}
-      />
-      <form
-        className="flex flex-col justify-between  items-start gap-[50px] mx-[auto] p-[14px] max-w-[453px]  bg-slate-100 mt-[150px]"
-        onSubmit={handleSubmit}
-      >
-        <h1 className="text-[36px] text-[#22269E] w">Welcome back!</h1>
+      <Suspense>
+        <NavBar />
+        <Image
+          className="absolute pt-[20px] -z-50 "
+          src="/images/assets.png"
+          alt="blackground"
+          width={1459.64}
+          height={839}
+          priority={true}
+        />
+        <form
+          className="flex flex-col justify-between  items-start gap-[50px] mx-[auto] p-[14px] max-w-[453px]  bg-slate-100 mt-[150px]"
+          onSubmit={handleSubmit}
+        >
+          <h1 className="text-[36px] text-[#22269E] w">Welcome back!</h1>
 
-        <div className="w-full">
-          <div>Email</div>
-          <input
-            name="email"
-            type="email"
-            className=" w-full h-[48px] p-[14px]"
-            placeholder="Enter Email"
-            onChange={(event) => {
-              setEmail(event.target.value);
-            }}
-            value={email}
-          ></input>
-        </div>
+          <div className="w-full">
+            <div>Email</div>
+            <input
+              name="email"
+              type="email"
+              className=" w-full h-[48px] p-[14px]"
+              placeholder="Enter Email"
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
+              value={email}
+            ></input>
+          </div>
 
-        <div className="w-full">
-          <div>Password</div>
-          <input
-            name="password"
-            className=" w-full h-[48px] p-[14px]"
-            placeholder="Enter Password"
-            type="password"
-            onChange={(event) => {
-              setPassword(event.target.value);
-            }}
-            value={password}
-          ></input>
+          <div className="w-full">
+            <div>Password</div>
+            <input
+              name="password"
+              className=" w-full h-[48px] p-[14px]"
+              placeholder="Enter Password"
+              type="password"
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+              value={password}
+            ></input>
 
-          {error && (
-            <div className="  text-red-600">Incorrect username or password</div>
-          )}
-        </div>
+            {error && (
+              <div className="  text-red-600">
+                Incorrect username or password
+              </div>
+            )}
+          </div>
 
-        <button className=" bg-slate-300 w-full h-[60px]" type="submit">
-          Log in
-        </button>
+          <button className=" bg-slate-300 w-full h-[60px]" type="submit">
+            Log in
+          </button>
 
-        <div className="flex gap-[5px]">
-          <div>Don’t have an account?</div>
-          <Link href="/register">
-            <div className=" text-[#2F5FAC] font-bold">Register</div>
-          </Link>
-        </div>
-      </form>
+          <div className="flex gap-[5px]">
+            <div>Don’t have an account?</div>
+            <Link href="/register">
+              <div className=" text-[#2F5FAC] font-bold">Register</div>
+            </Link>
+          </div>
+        </form>
+      </Suspense>
     </>
   );
 }

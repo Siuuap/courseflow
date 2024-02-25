7678676f04bd01cfff79ddd40aa0863300b8da78
tanasri <tanawan95@gmail.com>
2024-02-25 15:47:21 +0700

    feat:assignmentSection

--- src/app/api/learning/[course_id]/updateduedate/route.js
+++ src/app/api/learning/[course_id]/updateduedate/route.js
@@ -6,18 +6,20 @@ export async function POST(req, { params }) {
   const body = await req.json();
   const userId = searchParams.get("userid");
   const subLessonId = searchParams.get("sublessonid");
+
   const date = new Date();
+  date.setDate(date.getDate() + body.duration);
 
   const { data, error } = await supabase
     .from("users_sub_lessons")
     .update({
-      answer: body.answer,
+      due_date: date.toISOString(),
       status_assignment: body.status,
-      finished_date: date.toISOString(),
     })
     .eq("user_id", userId)
     .eq("sub_lesson_id", subLessonId)
     .select();
+
   return NextResponse.json(
     { message: "Update Successfully", data: data },
     { status: 200 }
